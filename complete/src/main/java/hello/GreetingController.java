27dcf05d313b2108c108c97f99a665eac0b7d6c2
Author daisy <daisyzs1213@163.com>
2019-09-28 17:22:47 +0800

    init

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+    private Long counter = 0L;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
+        return new Greeting(counter++,
                             String.format(template, name));
     }
 }
